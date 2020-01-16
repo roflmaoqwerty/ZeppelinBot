49c4af58a970ecb9c0b0ae9d79c3dc12335b8447
roflmaoqwerty <jacob_luke500@hotmail.com>
2020-01-16 19:02:34 +1100

    Created new migration for timed_unban table

--- backend/src/migrations/1579087839584-CreateBansTable.ts
+++ backend/src/migrations/1579087839584-CreateBansTable.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateBansTable1579087839584 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
